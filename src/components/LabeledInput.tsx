56e66d0ed1d920cec670be3d149af69cf85801b5
Marcin Kacperski <kacperski81@gmail.com>
2024-05-03 11:24:03 +0100

    useStateHook

--- src/components/LabeledInput.tsx
+++ src/components/LabeledInput.tsx
@@ -1,16 +1,19 @@
 interface LabeledInputProps {
     label: string;
     value: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export function LabeledInput({label, value} : LabeledInputProps) {
+export function LabeledInput({label, value, onChange} : LabeledInputProps) {
     return (
         <div className="flex flex-col m-2">
             <label>{label}</label>
             <input 
                 type="text"
+                className={"border border-gray-400 p-1"}
                 value={value}
-                className={"border border-gray-400 p-1"} />
+                onChange={onChange}
+            />
         </div>
     )
 }
--- src/person-editor/PersonEditor.tsx
+++ src/person-editor/PersonEditor.tsx
@@ -2,45 +2,76 @@ import { useState } from 'react';
 import { LabeledInput } from '../components'
 import { initialPerson } from '../utils/InitialPerson';
 
+
 export function PersonEditor() {
-    const [person, setPerson] = useState(initialPerson);
+    const [person, setPerson] = useState(() => initialPerson);
 
     return (
         <section className="max-w-full">
+
             <form>
+
                 <LabeledInput
                     label="First Name"
                     value={person.firstname}
+                    onChange={(e) => {
+                        setPerson((state) => ({...state,firstname: e.target.value}));
+                    }}
                 />
+
                 <LabeledInput
                     label="Surname"
                     value={person.surname}
+                    onChange={(e) => {
+                        setPerson((state) => ({...state,surname: e.target.value}));
+                    }}
                 />
+
                 <LabeledInput
                     label="Email"
                     value={person.email}
+                    onChange={(e) => {
+                        setPerson((state) => ({...state,email: e.target.value}));
+                    }}
                 />
+
                 <LabeledInput
                     label="Address"
                     value={person.address}
+                    onChange={(e) => {
+                        setPerson((state) => ({...state,address: e.target.value}));
+                    }}
                 />
+
                 <LabeledInput
                     label="Phone"
                     value={person.phone}
+                    onChange={(e) => {
+                        setPerson((state) => ({...state,phone: e.target.value}));
+                    }}
                 />
+
                 <button
                     type="submit"
                     className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2"
-                >Submit</button>
+                >
+                    Submit
+                </button>
+
             </form>
+
             <hr />
+
             <p className="m-2">
+
                 <pre
                     className="overflow-hidden"
                 >
                     {JSON.stringify(person, null, 2)}
                 </pre>
+
             </p>
+
         </section>
     )
 }
